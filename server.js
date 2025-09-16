9105d491bb1dce987d1ba5e1c0c85d2610bcef12
Anustha Pal <anusthapal@gmail.com>
2025-09-16 11:53:41 +0530

    node js files

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = 80; 
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World from Node.js!");
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
